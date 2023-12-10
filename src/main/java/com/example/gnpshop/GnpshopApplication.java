5da6415173e6ce9d99960dc59f0f2a4b92d3085e
Gabi <gabrysia.czajkowska@wp.pl>
2023-12-10 11:05:27 +0100

    first commit

---
+++ src/main/java/com/example/gnpshop/GnpshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.gnpshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GnpshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GnpshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gnpshop/GnpshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gnpshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GnpshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
